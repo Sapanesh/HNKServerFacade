302379ceb5e3a277eb79ba01811a9174b41ec271
harlan <hkellaway.tech@gmail.com>
2015-04-13 19:24:04 -0700

    Installed next pod version; updated .travis

---
+++ Example/Pods/Target Support Files/Pods-HNKServerFacade-Example/Pods-HNKServerFacade-Example-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// HNKServerFacade
+#define COCOAPODS_POD_AVAILABLE_HNKServerFacade
+#define COCOAPODS_VERSION_MAJOR_HNKServerFacade 0
+#define COCOAPODS_VERSION_MINOR_HNKServerFacade 0
+#define COCOAPODS_VERSION_PATCH_HNKServerFacade 1
+
